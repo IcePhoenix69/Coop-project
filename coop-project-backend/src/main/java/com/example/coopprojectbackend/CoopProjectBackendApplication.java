da7d29d2e27ac701d2c0c786db7bc073deee9a1c
IcePhoenix69 <matteo.ventura@refresh-academy.org>
2025-12-29 11:16:09 +0100

    Creazione progetto parte back-end

---
+++ coop-project-backend/src/main/java/com/example/coopprojectbackend/CoopProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coopprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopProjectBackendApplication.class, args);
+    }
+
+}
---
+++ coop-project-backend/src/test/java/com/example/coopprojectbackend/CoopProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coopprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
